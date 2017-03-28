b1b7bf7769111fb5694e91049f4d49ec4eb19cec
Edwin Amsler <edwinguy@gmail.com>
2017-03-28 07:03:41 +0000

    Allow anyone to access GPIO. Not safe, but convenient ATM

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 for io in `seq 132 139`; do
 	echo $io > /sys/class/gpio/export
 	echo out > /sys/class/gpio/gpio$io/direction
+	chmod 666 /sys/class/gpio/gpio$io/value
 done
 
 target/debug/garage-tools
